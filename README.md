20f04e516dee6ef37c4a52e5fa6403c63691ce11
Claudia Hernandez <claudiahdez13@gmail.com>
2021-01-19 19:36:09 -0800

    final changes to server.js and final readme push

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # NoteTaker
+This template is for a Note Taker application that allows the user to easily write down notes they need. This template was created using express node.js and Javascript.
+
+ This app makes it easy for the user to write down their thoughts or pending things to do. When the user exits the app the notes written down are saved via the db.json file that makes it impossible for the user's notes to disapear unless the user deletes the note themself. When the user is finished with a specific note they can just click on the trash button next to the note to delete the note, this is the only way the note can disapear on the app.
+
+ The code below is the code used to get the information the user types out on the app before being able to retrieve it by using app.post code: 
+ ```js
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+```
+
+![NoteTakerIMG](assets/Note-Taker-app.png)
+
+[GitHub Repo] (https://github.com/Claudialhc/NoteTaker)
+Heroku Deployed URL: 
--- public/assets/server.js
+++ public/assets/server.js
@@ -15,9 +15,6 @@ app.use(express.static("public")); //tells the express app where the static asse
 
 // GET * - Should return the index.html file
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, './public/index.html'));
-// });
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
@@ -25,33 +22,17 @@ app.get("/notes", (req, res) => {
 
 
 // The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
-
-
 // The following API routes should be created:
-
-
-// GET /api/notes - Should read the db.json file and return all saved notes as JSON. fs.readFile!!!!
+// GET /api/notes - Should read the db.json file and return all saved notes as JSON. 
 
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, "/db/db.json"));
 });
-// app.get('/api/notes', (req, res) => {
-//   fs.readFile('./db/db.json', function(err, data){
-//     if (err) console.log(JSON.parse(data));
-//     res.json(data);
-//   })
-// });
+
 
 // POST /api/notes - Should receive a new note to save on the request body (req.body!!!!),
 // add it to the db.json file, and then return the new note to the client.
-// here you'll need to fs.readFile and writeFile, do both, in that order yay
 
-// app.post("/api/reserve", (req, res) => {
-//   console.log(req.body);
-//   const newNote = req.body;
-//   fs.readFile('./db/db.json', "utf8", function(err, data){
-//     if (err) console.log(JSON.parse(data));
-//     res.json(data);
 
 app.post("/api/notes", function(req, res) {
   const savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -63,11 +44,8 @@ app.post("/api/notes", function(req, res) {
   console.log("Note saved to db.json. Content: ", newNote);
   res.json(savedNotes);
 })
-// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. (req.params.id)
-//This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, 
-//you'll need to read all notes from the db.json file, remove the note with the given id property, 
-//and then rewrite the notes to the db.json file.
-// you'll also need to fs.readFile and writeFile in that order here
+
+// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete.
 
 app.delete("/api/notes/:id", function(req, res) {
   let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -94,8 +72,3 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
 });
-
-// Acceptance Criteria
-// Application should allow users to create and save notes.
-// Application should allow users to view previously saved notes.
-// Application should allow users to delete previously saved notes.
